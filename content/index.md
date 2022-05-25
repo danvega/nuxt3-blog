3a818d2e00f87c44e9e7bde6d5f8e24be3bf9fd0
Dan Vega <danvega@gmail.com>
2022-05-25 09:33:37 -0400

    init commit

---
+++ content/index.md
@@ -0,0 +1,25 @@
+---
+title: My First Markdown Page!
+---
+
+# Hello, Markdown!
+
+This is a paragraph
+
+This is a new paragraph
+
+```bash
+# yarn
+yarn install
+
+# npm
+npm install
+```
+
+```
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
+```
---
+++ nuxt.config.ts
@@ -0,0 +1,14 @@
+import { defineNuxtConfig } from 'nuxt'
+
+// https://v3.nuxtjs.org/api/configuration/nuxt.config
+export default defineNuxtConfig({
+  modules: [
+    '@nuxt/content'
+  ],
+  content: {
+    highlight: {
+      // See the available themes on https://github.com/shikijs/shiki/blob/main/docs/themes.md#all-theme
+      theme: 'nord'
+    }
+  }
+})
